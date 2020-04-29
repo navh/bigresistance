aaf7e954d4e9372b45d6ee189ef360aa0831f4d5
navh <navarre.hebb@gmail.com>
2020-04-28 22:49:11 -0300

    Entire tic tac toe app just to try to figure out how to wire together the database

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <section>
+    <h1>Tic Tac Toe</h1>
+    <Login v-if="!user" v-on:login="addUser" />
+    <SelectGame
+      v-if="user"
+      v-on:create-game="createGame"
+      v-on:join-game="joinGame"
+    />
+    <hr />
+    <Gameboard :user="user" v-if="activeGame" :gameId="activeGame" />
+  </section>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { database } from "firebase";
+import uuid from "uuid";
+import board from "./board";
+import Gameboard from "./components/Gameboard";
+import Login from "./components/Login";
+import SelectGame from "./components/SelectGame";
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  name: "app",
+  components: { Gameboard, Login, SelectGame },
+  data: () => ({
+    user: null,
+    allUsers: [],
+    activeGame: null
+  }),
+  methods: {
+    addUser(name) {
+      const userObject = this.allUsers.find(u => u.name === name);
+      if (userObject) {
+        this.user = userObject;
+        return;
+      }
+      const newUser = { name, games: [] };
+      database()
+        .ref(`users`)
+        .set([...this.allUsers, newUser]);
+      this.user = newUser;
+    },
+    createGame(name) {
+      const id = uuid();
+      const newGame = { creator: this.user.name, name, id, board };
+      database()
+        .ref(`/games/${id}`)
+        .set(newGame);
+      this.activeGame = id;
+    },
+    joinGame(id) {
+      this.activeGame = id;
+    }
+  },
+  created() {
+    database()
+      .ref("users")
+      .on("value", snapshot => {
+        if (snapshot.val()) {
+          this.allUsers = snapshot.val();
+        }
+      });
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
